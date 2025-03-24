aa61ee6cf9eb4d70613b93c8b8b9b5fb2cd0a118
Iyanu Ajimobi <iyanuajimobi12@gmail.com>
2025-03-24 11:57:04 +0100

    initialize frontend project with Vite, React, and TypeScript setup

---
+++ frontend/jest.config.ts
@@ -0,0 +1,20 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.jest.json',
+      useESM: true,
+      jsx: 'react-jsx'
+    }]
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  verbose: true
+};
